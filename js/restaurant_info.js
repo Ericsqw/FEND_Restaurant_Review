6ee23ffe70b35f2fa8952abe1c9a827348ff2c50
Ericsqw <ishenqiwei@gmail.com>
2018-10-14 08:53:47 +1000

    Feature: Add map token
    - Mapbox tokens are added to both main.js and restaurant.js files.
    -Tested and worked.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiZXJpY3NxdyIsImEiOiJjam44MTl2MmIwNGJ3M2tveHprZW01Y2p1In0.UT3fEul5k4o-sWapwb3EfQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
