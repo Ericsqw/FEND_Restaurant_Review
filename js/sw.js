df01608e2211fbfd3f458d2a92c1b1a2243fd1ca
Ericsqw <ishenqiwei@gmail.com>
2018-10-15 14:18:48 +1000

    Feature: Service Worker
    Add a ServiceWorker script to cache requests to all of the site’s assets so that any page that has been visited by a user will be accessible when the user is offline.

--- js/sw.js
+++ js/sw.js
@@ -26,3 +26,27 @@ self.addEventListener('install', function(e) {
 		);
 });
 
+	self.addEventListener('fetch', function(e) {
+		e.respondWidth(
+			caches.match(e.request).then(function(response) {
+				if (response) {
+					console.log('Found', e.request, 'in cache');
+					return response;
+				}
+				else {
+					console.log('Could not find', e.request, 'in cache, FETCHING');
+					return fetch(e.request)
+					.then(function(response) {
+						const cloneResponse = response.clone();
+						caches.open('v1').then(function(cache) {
+							cache.put(e.request, clonedResponse);
+						})
+						return response;
+					})
+					.catch(function(err) {
+						console.error(err);
+					});
+				}
+			})
+		);
+	});
